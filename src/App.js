9b09db9d61aa8882cac469f6b82b95009731334b
Crong (SungEun Kim) <mirxx@naver.com>
2022-06-25 23:42:12 +0900

    components 분리

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import styled from '@emotion/styled';
+
 import TodoContext from './components/TodoContext';
+
 import TodoCreate from './components/TodoCreate';
 import TodoHead from './components/TodoHead';
 import TodoList from './components/TodoList';
+
 import TodoTemplate from './components/TodoTemplate';
 
 const Background = styled.div({
